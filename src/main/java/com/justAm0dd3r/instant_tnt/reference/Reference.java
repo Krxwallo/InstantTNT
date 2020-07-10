a655e20601ab5d578bc75d9677e4782feb6e2160
Krxwallo <a@b.c>
2020-07-10 15:59:04 +0200

    Update Reference class - Version 1.1.0

--- src/main/java/com/justAm0dd3r/instant_tnt/reference/Reference.java
+++ src/main/java/com/justAm0dd3r/instant_tnt/reference/Reference.java
@@ -6,7 +6,7 @@ package com.justAm0dd3r.instant_tnt.reference;
 public class Reference {
     public static final String MOD_NAME = "Instant TNT";
     public static final String MOD_ID = "instant_tnt";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
     public static final String AUTHOR = "justAm0dd3r";
 
 }
