ffaf00a74af54667778861c85184dc515bece735
Krxwallo <a@b.c>
2020-07-10 13:00:02 +0200

    Added Reference class.

---
+++ src/main/java/com/justAm0dd3r/instant_tnt/InstantTNT.java
@@ -0,0 +1,33 @@
+package com.justAm0dd3r.instant_tnt;
+
+import com.justAm0dd3r.instant_tnt.reference.Reference;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.Ref;
+import java.util.stream.Collectors;
+
+/**
+ * Author: justAm0dd3r
+ */
+@Mod(Reference.MOD_ID)
+public class InstantTNT {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public InstantTNT() {
+        LOGGER.info(Reference.MOD_NAME + " Version " + Reference.VERSION + " by " + Reference.AUTHOR + " started up.");
+    }
+}
